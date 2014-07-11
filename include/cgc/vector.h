14ba2a6f1a697f84a7a1376839cae4a3a3739a5a
Harenome Ranaivoarivony Razanajato <harno.ranaivo@gmail.com>
2014-07-11 18:20:30 +0200

    cgc_vector: update doc

--- include/cgc/vector.h
+++ include/cgc/vector.h
@@ -38,7 +38,104 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * \brief CGC vector
+ * \brief CGC vector.
+ *
+ * CGC Vectors are generic vectors. Elements are stored contiguously inside
+ * the vector.
+ *
+ * # Basics
+ * To achieve genericity, CGC Vectors rely on 3 things:
+ * - the size in bytes of the elements
+ * - a copy function
+ * - a cleaning function
+ *
+ * It is up to the user to properly write these 2 functions, if needed.
+ *
+ * Since elements are stored contiguously, the vector allocates memory for
+ * several elements, and grows when more memory is needed.
+ *
+ * ## Element allocation
+ * CGC Vectors allocate memory for several elements. If a size is provided at
+ * the vector creation, it will be used as the default step for the vector's
+ * growth (see cgc_vector_new()). Otherwise, the vector growth will use an
+ * arbitrary step.
+ *
+ * ## Copy function
+ * Since the CGC Vector could possibly hold any type of element, it might be
+ * necessary to supply a way to properly copy elements into the vector.
+ *
+ * If no copy function was provided at the vector creation, a mere copy (using
+ * \c memcpy) of the provided element will be done.
+ *
+ * ## Cleaning function
+ * A cleaning function may be required since the CGC Vector aims to be generic:
+ * users should be able to store any type of element in the list. Elements could
+ * themselves point to dynamically allocated memory. Thus this function shall,
+ * if needed, free any memory an element of the list could supposedly hold.
+ *
+ * Note that the function should free any dynamically allocated memory held by
+ * the element, but should not free the aforementioned element.
+ *
+ * # CGC Vector creation and destruction
+ * ## Dynamic allocation
+ * CGC Vectors can be dynamically created using cgc_vector_new(). Such vectors
+ * must be destroyed using cgc_vector_free().
+ *
+ * # Vector properties
+ * One can determine whether a vector is empty using cgc_vector_is_empty(), get
+ * its current size using cgc_vector_size(), and its current maximum size (the
+ * size at which a resizing operation will occur) with cgc_vector_max_size().
+ *
+ * # Element access
+ * ## Without changing the vector
+ * There are three functions that return pointers to elements of the vector.
+ * Please be aware that the pointers directly point to an element of the vector:
+ * modifying it should be done with extreme care.
+ *
+ * Access function     | Pointer to
+ * --------------------|---------------
+ * cgc_vector_at()     | Nth element
+ * cgc_vector_front()  | First element
+ * cgc_vector_back()   | Last element
+ *
+ * Note that these functions do not:
+ * - check whether the supplied vector is valid
+ * - check whether the requested element exists.
+ *
+ * ## While modifying the vector
+ * It is also possible to get an element while removing it from the vector.
+ *
+ * \b Important: Elements accessed in this fashion shall be freed by the user.
+ *
+ * Access function         | Element
+ * ------------------------|----------
+ * cgc_vector_pop_front()  | First element
+ * cgc_vector_pop_back()   | Last element
+ *
+ * Note that removing the first element implies shifting the remainder of the
+ * vector. If numerous removals of the first element are planned, consider
+ * using cgc_list instead of cgc_vector.
+ *
+ * # Modification
+ * ## Insertions
+ *
+ * Insertion function         | Insertion
+ * ---------------------------|------------------------
+ * cgc_vector_push_front()    | As the first element
+ * cgc_vector_push_back()     | As the last element
+ * cgc_vector_insert()        | Before the Nth element
+ *
+ * Note that insertions anywhere else than the back imply shifting the remainder
+ * of the vector. If such insertions are to be used numerous times, consider
+ * using cgc_list instead of cgc_vector.
+ *
+ * ## Deletions
+ * Deletion function         | Deleted elements
+ * --------------------------|-----------------------------
+ * cgc_vector_pop_front()    | First element
+ * cgc_vector_pop_back()     | Last element
+ * cgc_vector_erase()        | Several contiguous elements
+ * cgc_vector_clear()        | All elements
  */
 typedef struct cgc_vector
 {
@@ -63,6 +160,10 @@ typedef struct cgc_vector
  * \param size Vector size.
  * \relatesalso cgc_vector
  * \return pointer to a cgc_vector.
+ * \retval NULL if the vector could not be allocated.
+ * \note Vectors obtained this way must be detroyed using cgc_vector_free().
+ * \note In case of failure, \c errno may be set to \c ENOMEM.
+ * \note A call to this function may change the value of \c errno.
  */
 cgc_vector * cgc_vector_new (size_t element_size, cgc_copy_function copy_fun, cgc_clean_function clean_fun, size_t size);
 
@@ -70,6 +171,9 @@ cgc_vector * cgc_vector_new (size_t element_size, cgc_copy_function copy_fun, cg
  * \brief Free a cgc_vector.
  * \param vector Vector.
  * \relatesalso cgc_vector
+ * \note It is safe to pass a \c NULL pointer to this function.
+ * \warning It is strongly advised to use this function only on vectors obtained
+ * via cgc_vector_new().
  */
 void cgc_vector_free (cgc_vector * vector);
 
@@ -83,6 +187,7 @@ void cgc_vector_free (cgc_vector * vector);
  * \retval true if the vector is empty.
  * \retval false otherwise.
  * \relatesalso cgc_vector
+ * \pre \c vector != \c NULL
  */
 bool cgc_vector_is_empty (const cgc_vector * vector);
 
@@ -91,6 +196,7 @@ bool cgc_vector_is_empty (const cgc_vector * vector);
  * \param vector Vector.
  * \return Size.
  * \relatesalso cgc_vector
+ * \pre \c vector != \c NULL
  */
 size_t cgc_vector_size (const cgc_vector * vector);
 
@@ -99,6 +205,7 @@ size_t cgc_vector_size (const cgc_vector * vector);
  * \param vector Vector.
  * \return Max size.
  * \relatesalso cgc_vector
+ * \pre \c vector != \c NULL
  */
 size_t cgc_vector_max_size (const cgc_vector * vector);
 
@@ -112,6 +219,11 @@ size_t cgc_vector_max_size (const cgc_vector * vector);
  * \param i Index.
  * \return element.
  * \relatesalso cgc_vector
+ * \pre cgc_vector_size(vector) > \c i
+ * \pre \c vector != \c NULL
+ * \warning This function does not check whether the supplied vector is valid!
+ * \warning This function does not check whether the supplied vector is long
+ * enough!
  */
 void * cgc_vector_at (const cgc_vector * vector, size_t i);
 
@@ -120,6 +232,11 @@ void * cgc_vector_at (const cgc_vector * vector, size_t i);
  * \param vector Vector.
  * \return First element.
  * \relatesalso cgc_vector
+ * \pre cgc_vector_is_empty(vector) == \c false
+ * \pre \c vector != \c NULL
+ * \warning This function does not check whether the supplied vector is valid!
+ * \warning This function does not check whether the supplied vector is long
+ * enough!
  */
 void * cgc_vector_front (const cgc_vector * vector);
 
@@ -128,6 +245,11 @@ void * cgc_vector_front (const cgc_vector * vector);
  * \param vector Vector.
  * \return Last element.
  * \relatesalso cgc_vector
+ * \pre cgc_vector_is_empty(vector) == \c false
+ * \pre \c vector != \c NULL
+ * \warning This function does not check whether the supplied vector is valid!
+ * \warning This function does not check whether the supplied vector is long
+ * enough!
  */
 void * cgc_vector_back (const cgc_vector * vector);
 
