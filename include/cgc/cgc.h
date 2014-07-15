deb439369cc91d2a3131688e9abef40668c3d0fd
Harenome Ranaivoarivony Razanajato <harno.ranaivo@gmail.com>
2014-07-15 23:28:11 +0200

    add cgc.h header

---
+++ include/cgc/cgc.h
@@ -0,0 +1,36 @@
+/**
+ * \file cgc.h
+ * \author RAZANAJATO RANAIVOARIVONY Harenome
+ * \date 2014
+ * \copyright LGPLv3
+ */
+/* Copyright © 2014 RAZANAJATO RANAIVOARIVONY Harenome.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * and the GNU General Public License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+ #ifndef _CGC_H_
+ #define _CGC_H_
+
+/* Include everything. */
+#include "cgc/common.h"
+#include "cgc/types.h"
+#include "cgc/list.h"
+#include "cgc/queue.h"
+#include "cgc/stack.h"
+#include "cgc/vector.h"
+#include "cgc/version.h"
+
+ #endif /* _CGC_H_ */
